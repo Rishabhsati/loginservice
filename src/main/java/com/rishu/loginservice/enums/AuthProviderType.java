f0ae399027ec04c1685c98d1ed4a1137c52f4b0f
Rishabhsati <rishabhsati6505@gmail.com>
2026-01-29 23:46:12 +0530

    added jwt and some dependency

---
+++ src/main/java/com/rishu/loginservice/enums/AuthProviderType.java
@@ -0,0 +1,9 @@
+package com.rishu.loginservice.enums;
+
+public enum AuthProviderType {
+    GOOGLE,
+    GITHUB,
+    FACEBOOK,
+    TWITTER,
+    EMAIL
+}
---
+++ src/main/java/com/rishu/loginservice/enums/RoleType.java
@@ -0,0 +1,6 @@
+package com.rishu.loginservice.enums;
+
+public enum RoleType {
+    ADMIN,
+    USER
+}
